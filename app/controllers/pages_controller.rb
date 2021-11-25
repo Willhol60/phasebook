a82cef7a6dbc7878e2a99e0f6fc20a4d2fc1ceb8
simonfoster <simonfos74@gmail.com>
2021-11-25 10:19:34 +0000

    user changes so far

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,40 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  def index
+    @user = current_user
+    @readings = Reading.where("user_id = ?", params[:user_id])
+  end
 
-  def home
+  def profile
+    @user = current_user
+  end
+
+  # def books_read
+  #   return unless @reading.read_status == 'Future'
+
+  #   total = 0
+  #   @readings.each do |reading|
+  #     books = reading.book.count
+  #     total += books
+  #   end
+  # end
+
+  def time_spent
+    time = 0
+    @readings.each do |reading|
+      duration = reading.end_date - reading.start_date
+      time += duration
+    end
+  end
+
+  def avg_speed
+    time_spent / books_read
+  end
+
+  def categories
+    total = 0
+    @readings.each do |reading|
+      cat = reading.book.category.count
+      total += cat
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'comments/edit'
   get 'comments/update'
   get 'comments/destroy'
+  get '/profile', to: 'pages#profile'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

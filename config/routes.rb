4bd7b33e8bffeef07f69695b34af95959f2fb8a2
Will Holmes <willholmes60@gmail.com>
2021-11-30 12:33:28 +0000

    new reading window

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-  get 'comments/create'
-  get 'comments/edit'
-  get 'comments/update'
-  get 'comments/destroy'
   get '/profile', to: 'pages#profile'
   devise_for :users
   root to: 'pages#home'

26e0cf992dfa58ea8a0f746bfffeb04858e50eaa
Will Holmes <willholmes60@gmail.com>
2021-12-02 15:16:58 +0000

    layout changes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,7 +36,7 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to books_path
+    redirect_to books_path(anchor: "categories")
   end
 
   def random
